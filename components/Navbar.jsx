2f7e4a0af541a73b649d42279626b0384890ff64
remcostoeten <remcostoeten@hotmail.com>
2023-04-17 23:28:27 +0200

    Add base of header

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,17 +4,15 @@ import { GitHub } from '@mui/icons-material';
 
 export default function Navbar() {
 	return (
-		<nav className='header'>
-			<div className='header__inner'>
-				<ul className='header__left'>
-					<li>Pricing</li>
-					<li>Websocket</li>
-					<li>Contact</li>
-				</ul>
-				<div className='header__right'>
-					<DiscordSvg />
-					<GitHub />
-				</div>
+		<nav className='header container'>
+			<ul className='header__left'>
+				<li>Pricing</li>
+				<li>Websocket</li>
+				<li>Contact</li>
+			</ul>
+			<div className='header__right'>
+				<DiscordSvg />
+				<GitHub />
 			</div>
 		</nav>
 	);
