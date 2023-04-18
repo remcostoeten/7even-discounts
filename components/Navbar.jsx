04cb64e7fab79271e692e1384f7bb5260ad7f323
remcostoeten <remcostoeten@hotmail.com>
2023-04-18 13:09:47 +0200

    Add stuff

---
+++ components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import DiscordSvg from './svg/discord';
+import SwipeableTemporaryDrawer from './Offcanvas';
+import { useMediaQuery } from '@mui/material';
+
+export default function Navbar() {
+	const isMobile = useMediaQuery('(max-width:768px)');
+	return (
+		<>
+			{isMobile ? (
+				<div className='offcanvas-menu'>
+					<div className='offcanvas-menu__menu'>
+						<SwipeableTemporaryDrawer />
+					</div>
+				</div>
+			) : (
+				<nav className='header container'>
+					<ul className='header__left'>
+						<li>Home</li>
+						<li>Pricing</li>
+						<li>Websocket</li>
+						<li>Contact</li>
+					</ul>
+					<div className='header__right'>
+						<DiscordSvg />
+					</div>
+				</nav>
+			)}
+		</>
+	);
+}
