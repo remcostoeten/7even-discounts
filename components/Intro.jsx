b30adfc753bc964f6b2bf588565632d99b4e68ba
remcostoeten <remcostoeten@hotmail.com>
2023-04-18 15:22:26 +0200

    Intro

---
+++ components/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Intro() {
+	return (
+		<>
+			<div className='intro container'>
+				<h1>Welcome,</h1>
+				<p>
+					<span>7even discounts</span>
+				</p>
+				<p class='fill' data-text='Lame slogan                                           '>
+					Lame slogan
+				</p>
+			</div>
+		</>
+	);
+}
